837601bcf6b18c70ca0138a0f9a6941f5334d22f
ashwinprashant <ashwin.prashant@kellogg.northwestern.edu>
2022-05-05 02:54:22 +0000

    final edits

--- kmdb.rb
+++ kmdb.rb
@@ -295,16 +295,14 @@ puts "Movies"
 puts "======"
 
 
-
-#movies = Movie.where({"studio_id" => warner_bros["id"]})
 movies = Movie.all
 
 for movie in movies
     movie_title = movie["title"]
     movie_year_released = movie["year_released"]
     movie_rated = movie["rated"]
-    #studio_name = warner_bros["name"]
-    studio_name = movie.Studio["name"]
+    studio = Studio.find_by({"id" => movie["studio_id"]})
+    studio_name = studio["name"]
 puts "#{movie_title} | #{movie_year_released} | #{movie_rated} | #{studio_name}"
 end
 
@@ -313,18 +311,16 @@ puts "Top Cast"
 puts "========"
 puts ""
 
-#movies = Movie.where({"studio_id" => warner_bros["id"]})
 movies = Movie.all
 for movie in movies
     movie_title = movie["title"]
     movie_number = movie["id"]
     roles = Role.where({"movie_id" => movie["id"]})
         for role in roles
-            #actor_number = role["actor_id"]
+            actor_number = role["actor_id"]
             name_of_character = role["character_name"]
-            #actors = Actor.find_by({"id" => role["actor_id"]})
-            #actor_name = actors["name"]
-            actor_name = role.actor_id["name"]
+            actors = Actor.find_by({"id" => role["actor_id"]})
+            actor_name = actors["name"]
             puts "#{movie_title} | #{actor_name} | #{name_of_character}"
         end
 end
